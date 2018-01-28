d0928d59dd47d4916cdf31c0d84527741fc6c167
Noy Meir <MY@MY-PC>
2018-01-28 15:55:56 +0200

    Release 2

---
+++ src/ConsoleAppCore/Program.cs
@@ -0,0 +1,48 @@
+﻿using ImageSharp;
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace ConsoleAppCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var argsParser = new CommandLineParser(args);
+            string src = argsParser["src"];
+            string dest = argsParser["dest"];
+            dest = Path.Combine(AppContext.BaseDirectory,
+                                dest,
+                                DateTime.Now.ToString("HH-mm-ss"));
+            if (!Directory.Exists(dest))
+                Directory.CreateDirectory(dest);
+
+            var paths = Directory.GetFiles(src);
+            var sw = Stopwatch.StartNew();
+            foreach (var path in paths)
+            {
+                ProcessImage(dest, path);
+            }
+
+            sw.Stop();
+            Console.WriteLine($"Done: {sw.Elapsed}");
+            Console.ReadKey();
+        }
+
+        private static void ProcessImage(string dest, string path)
+        {
+            byte[] photoBytes = File.ReadAllBytes(path);
+            string name = Path.GetFileName(path);
+            string target = $@"{dest}\{name}.jpg";
+            Console.WriteLine(name);
+
+            using (var outStream = new FileStream(target, FileMode.Create))
+            {
+                var img = new Image(photoBytes);
+                img.Resize(new Size(600, 600)).Grayscale().Save(outStream);
+            }
+        }
+
+    }
+}
